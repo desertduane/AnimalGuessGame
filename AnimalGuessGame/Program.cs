5e070c50813be0663e19cd0728ac51e178fb7e37
duane rush <desertduane@yahoo.com>
2017-10-20 16:12:34 -0700

    Add project files.

---
+++ AnimalGuessGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static AnimalGuessGame.SearchTree;
+
+namespace AnimalGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchTree testTree = new SearchTree("Is it a Mammal?");
+            //testTree.Root.Left = new Node("Does it Bark?")
+            //{
+            //    Left = new Node("Dog"),
+            //    Right = new Node("Cat")
+            //};
+
+            //testTree.Root.Right = new Node("Can it fly?")
+            //{
+            //    Left = new Node("Bird"),
+            //    Right = new Node("Fish")
+            //};
+
+            testTree.Question(testTree.Root);
+
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ AnimalGuessGame/SearchTree.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalGuessGame
+{
+    class SearchTree
+    {
+        public Node Root { get; set; }
+
+        public SearchTree(string data)
+        {
+            Root = new Node(data);
+        }
+
+        public void Question(Node Current)
+        {
+            if (Current.Left == null && Current.Right == null)
+            {
+                Guess(Current);
+            }
+            else
+            {
+                Console.WriteLine($"{ Current.Data}");
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y" || answer == "yes")
+                {
+                    Question(Current.Left);
+                }
+                else
+                {
+                    Question(Current.Right);
+                }
+            }
+        }
+        public void Guess(Node node)
+        {
+            Console.WriteLine($"Is is a {node.Data}?");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "y" || answer == "yes")
+            {
+                Console.WriteLine("YAY");
+            }
+            else
+            {
+                Console.WriteLine("What animal did you have in mind?");
+                string newAnimal = Console.ReadLine();
+                Console.WriteLine($"Give a Question to help me guess {node.Data}");
+                string newQuestion = Console.ReadLine();
+                node.Right = new Node(newAnimal);
+                node.Left = new Node(node.Data);
+                node.Data = newQuestion;
+
+                TryAgain();
+            }
+        }
+
+        public void TryAgain()
+        {
+            Console.WriteLine("Play again?");
+            string answer = Console.ReadLine().ToLower();
+            if(answer == "y" || answer == "yes")
+            {
+                Question(Root);
+            }
+            else
+            {
+                Console.WriteLine("Ill get it next time");
+                return;
+            }
+        }
+
+
+
+       
+        public class Node
+        {
+            public string Data { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public Node Current { get; set; }
+
+            public Node(string data)
+            {
+                Data = data;
+                Left = null;
+                Right = null;
+            }
+        }
+    }
+}
